3d8d49c32259e3577d96e22af4887fb9796b302a
Isaac Tessler <ijt3cd@virginia.edu>
2015-06-23 20:26:18 +0200

    geocoder, prepare for landing

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -5,6 +5,15 @@ class Suggestion < ActiveRecord::Base
 	validates :user_id, :presence => true
 		validates :overview, :presence => true, :length => {:maximum => 100}
 		validates :description, :presence => true
+
+
+	geocoded_by :address, :latitude  => :lat, :longitude => :lon
+	# geocoded_by :full_street_address   # can also be an IP address
+	after_validation :geocode          # auto-fetch coordinates
+
+	
+	
+
 end
 
 
