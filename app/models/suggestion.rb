45d1e4f968f9bac64eedf208ed187d1335531741
Isaac Tessler <ijt3cd@virginia.edu>
2015-06-23 20:30:09 +0200

    the eagle has landed (geocoder works)

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -7,7 +7,7 @@ class Suggestion < ActiveRecord::Base
 		validates :description, :presence => true
 
 
-	geocoded_by :address, :latitude  => :lat, :longitude => :lon
+	geocoded_by :location, :latitude  => :lat, :longitude => :lon
 	# geocoded_by :full_street_address   # can also be an IP address
 	after_validation :geocode          # auto-fetch coordinates
 
