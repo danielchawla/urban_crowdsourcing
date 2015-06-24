b7a2745ffe78f4d2d5bf9c3cb30850ac3c0ac74c
danielchawla <danielchawla93@gmail.com>
2015-06-25 00:03:14 +0200

    touch ups css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+    request.env['omniauth.origin'] || stored_location_for(resource) || suggestions_path
     # request.env['omniauth.origin'] || root_path
   end
   def after_sign_up_path_for(resource)
