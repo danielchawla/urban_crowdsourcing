bd271ebbb596fe6b16932a4bba86f125c3d3001f
Isaac Tessler <ijt3cd@virginia.edu>
2015-06-24 14:37:22 +0200

    main map working

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -7,6 +7,11 @@ class SuggestionsController < ApplicationController
 
 	def index
 		@suggestions = Suggestion.all.paginate(:page => params[:page], per_page: 3)
+		@suggestion_pins = Gmaps4rails.build_markers(@suggestions.geocoded) do |suggestion, marker|
+  			marker.lat suggestion.lat
+  			marker.lng suggestion.lon
+  			marker.infowindow render_to_string(:partial => "/suggestions/suggestion", :locals => {:suggestion => suggestion})
+		end
 	end
 
 	def new
