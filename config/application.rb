76191266da2edb59ef17c55bb9c448cc81003637
Isaac Tessler <ijt3cd@virginia.edu>
2015-06-24 23:53:13 +0200

    more config crap

--- config/application.rb
+++ config/application.rb
@@ -25,9 +25,9 @@ module UrbanCrowdsourcing
     # Do not swallow errors in after_commit/after_rollback callbacks.
    
     config.active_record.raise_in_transactional_callbacks = true
-    config.serve_static_assets = true
+    config.serve_static_files = true
     config.cache_classes = true
-config.serve_static_assets = true
+
 config.assets.compile = true
 config.assets.digest = true
   end
